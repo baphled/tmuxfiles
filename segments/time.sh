e1e074b2510cb7321f98d9436a7814a7c90e2f3f
Yomi Colledge <baphled@boodah.net>
2013-01-20 21:57:16 +0000

    Upgrade to the new version of tmux-powerline and setup the new customisations

---
+++ segments/time.sh
@@ -0,0 +1,7 @@
+run_segment() {
+  segment_path=$(dirname $0)
+
+  date +"⌚ %H:%M  "
+
+  return 0
+}
---
+++ segments/whoami.sh
@@ -0,0 +1,7 @@
+run_segment() {
+  segment_path=$(dirname $0)
+
+  echo "$(whoami)"
+
+  return 0
+}
